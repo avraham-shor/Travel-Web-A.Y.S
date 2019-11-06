acd4eb2e1be3896f55a6c7f17f82e952080238f0
avraham50 <stu@ravtech.local>
2019-11-06 15:03:37 +0200

    A. Y. Shor Travel web

--- docs/travel.js
+++ docs/travel.js
@@ -9,7 +9,7 @@ let destinations = [
     { code: 01, name: "Varsha", price: 190 },
     { code: 15, name: "Frankfort", price: 450 },
     { code: 07, name: "Levov", price: 220 },
-    { code: 20, name: "Vilenuse", price: 320 },
+    { code: 20, name: "Vilnius", price: 320 },
     { code: 31, name: "Turkay", price: 150 },
 ]
 let destHTML = document.querySelector('.destination tbody')
@@ -38,7 +38,7 @@ add_order('avraham', '012', 3, 62, destinations);
 add_order('moshe', '666', 2, 20, destinations);
 add_order('chaim', '123', 10, 11, destinations);
 add_order('pinchas', '033', 1, 1, destinations);
-add_order('dany', '828', 3, 31, destinations);
+add_order('דניאל', '828', 3, 31, destinations);
 add_order('Rubi', '207832692', 3, 7, destinations);
 add_order('a', '207832692', 3, 7, destinations);
 add_order('a', '207832692', 10, 11, destinations);
@@ -81,7 +81,7 @@ function add_order(name, zeut, pasengers, travel_id, destinations) {
         let travel = destinations.find(dItem => dItem.code == travel_id)
         let update = template_order
             .replace('#ID', orders.length + 1)
-            .replace('#NAME', name)
+            .replace('#NAME',uperCaseFirst(name))
             .replace('#ZEUT', zeut)
             .replace('#NAME', name)
             .replace('#PASENGERS', pasengers)
@@ -91,7 +91,7 @@ function add_order(name, zeut, pasengers, travel_id, destinations) {
         orderHTML.innerHTML += update;
 
         ordernung.push({
-            zeut: zeut, name: name, id_travel: travel_id, pasengers: pasengers,
+            zeut: zeut, name: uperCaseFirst(name), id_travel: travel_id, pasengers: pasengers,
             destination: travel.name, total_price: travel.price * pasengers
         })
 
@@ -117,7 +117,7 @@ function find() {
         let sumHtml = document.getElementById('sum_price_name');
         sumHtml.innerHTML = "";
         ordernung.forEach(ordItem => {
-            if (ordItem.name == user_find || ordItem.name == uperCaseFirst(user_find)) {
+            if (ordItem.name == uperCaseFirst(user_find)) {
 
                 update_find += template_find
                     .replace('#ZEUT', ordItem.zeut)
@@ -144,22 +144,18 @@ function find() {
 function find_destination() {
     console.log('function find_destination start');
     try {
-        let findHTML = document.querySelector('.select_destination tbody');
+        let findHTML = document.querySelector('.table_select_destination tbody');
         let template_find = `<tr><td>#ZEUT</td><td>#NAME</td>
         <td>#ID-TRAVEL</td><td>#DESTINATION</td><td>#PASSENGERS</td><td>$#TOTAL-PRICE</td></tr>`
 
         let update_find = "";
         let user_find = document.getElementById('select_destination').value;
-
+        let sum_price = 0;
+        let sumHtml = document.getElementById('sum_price_Destination');
         ordernung.forEach(ordItem => {
-            console.log("toLocaleUpperCase", user_find.toLocaleUpperCase(),
-                "toLocaleLowerCase", user_find.toLocaleLowerCase(),
-                "toLowerCase()", user_find.toLowerCase()
-
-
-            )
-            if (ordItem.destination == user_find || ordItem.destination == uperCaseFirst(user_find)) {
-
+            
+            if (ordItem.destination == user_find){
+               
                 update_find += template_find
                     .replace('#ZEUT', ordItem.zeut)
                     .replace('#NAME', ordItem.name)
@@ -167,10 +163,13 @@ function find_destination() {
                     .replace('#DESTINATION', ordItem.destination)
                     .replace('#PASSENGERS', ordItem.pasengers)
                     .replace('#TOTAL-PRICE', ordItem.total_price)
+                    console.log("update_find=",update_find)
+                    sum_price += ordItem.total_price;
             }
         });
 
         findHTML.innerHTML = update_find;
+        sumHtml.innerHTML = '<h2>$' + sum_price + '</h2>';
 
 
     } catch (error) {
